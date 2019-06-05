a8380bb33d8ffe9d7bd4213ab1f762ab414c7057
Trong Manh Tu <manh.trong@comarch.com>
2019-06-05 16:58:00 +0200

    Fix style of BurgerIcon

--- src/components/UI/BurgerIcon/index.styles.js
+++ src/components/UI/BurgerIcon/index.styles.js
@@ -6,17 +6,16 @@ export const StyledBurgerIcon = styled.div`
   top: 10px;
   right: 10px;
   width: 25px;
-  height: 15px;
   z-index: 600;
+  filter: drop-shadow(1px 1px 1px rgba(0, 0, 0, 0.7));
 
   path {
-    color: ${props => (props.open ? props.theme.primaryColor : '#ccc')};
+    fill: ${props => props.theme.primaryColor};
   }
 
   @media (min-width: ${mediaQueries.desktopMaxWidthLarge}) {
     left: 20px;
     right: unset;
     width: 30px;
-    height: 20px;
   }
 `;
--- src/components/Weather/WeatherWidget/index.styles.js
+++ src/components/Weather/WeatherWidget/index.styles.js
@@ -52,7 +52,7 @@ export const WeatherIcon = styled.div`
 
   @media (min-width: ${mediaQueries.desktopMaxWidthLarge}) {
     img {
-      transform: scale(1.8);
+      transform: scale(1.5);
     }
   }
 `;
