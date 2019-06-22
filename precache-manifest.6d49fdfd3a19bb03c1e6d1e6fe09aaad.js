4cb77c406981f74674815bb3f1bce23aa8428baa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 16:29:17 +0000

    Deploy maniekpl00/todo-weather-app to github.com/maniekpl00/todo-weather-app.git:gh-pages

---
+++ precache-manifest.6d49fdfd3a19bb03c1e6d1e6fe09aaad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531050a219e14e748e780ecd53b4a9c1",
+    "url": "/todo-weather-app/index.html"
+  },
+  {
+    "revision": "751f3b2d20b6d6f3719b",
+    "url": "/todo-weather-app/static/js/2.987a9fe2.chunk.js"
+  },
+  {
+    "revision": "95963d60960b13bcf103",
+    "url": "/todo-weather-app/static/js/main.14ef34a2.chunk.js"
+  },
+  {
+    "revision": "f2c1ad7fef7691b82338",
+    "url": "/todo-weather-app/static/js/runtime~main.f8b9f20c.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/todo-weather-app/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/todo-weather-app/static/media/times-solid.2c5552a1.svg"
+  }
+]);
